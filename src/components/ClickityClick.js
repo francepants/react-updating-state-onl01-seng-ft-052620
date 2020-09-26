ed6faf7eb98922adfc043af9534809906ae9d4f0
francepants <franceline90@gmail.com>
2020-09-26 11:21:25 -0500

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,30 @@
 // Code ClickityClick Component Here
+import React, { Component } from 'react'
+
+export class ClickityClick extends Component {
+    constructor() {
+        super()
+        //define initial state
+        this.state = {
+            hasBeenClicked: false
+        }
+    }
+
+    // To update our state, we use this.setState() and pass in an object. This object will get merged with the current state. When the state has been updated, our component re-renders automatically 
+    handleClick = () => {
+        this.setState({
+            hasBeenClicked: true
+        }, () => console.log(this.state.hasBeenClicked))
+    }
+
+    render() {
+        return (
+            <div>
+                <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+                <button onClick={this.handleClick}>Click me!</button>
+            </div>
+        )
+    }
+}
+
+export default ClickityClick
